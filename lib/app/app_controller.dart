cbcc3467095b51ab1a2948b82511863e284b2634
WeslleyFratini <weeslleey354@gmail.com>
2020-08-05 20:57:17 -0300

    viewModel

---
+++ lib/app/app_controller.dart
@@ -0,0 +1,20 @@
+import 'package:arch/app/interfaces/local_storage_interface.dart';
+import 'package:arch/app/services/shared_local_storage_service.dart';
+import 'package:arch/app/viewModels/change_theme_viewmodel.dart';
+import 'package:flutter/foundation.dart';
+
+class AppController {
+  static final AppController instance = AppController._();
+  AppController._() {
+    changeThemeViewModel.init();
+  }
+
+  final ChangeThemeViewModel changeThemeViewModel =
+      ChangeThemeViewModel(storage: SharedLocalStorageService());
+
+  bool get isDark => changeThemeViewModel.config.themeSwitch.value;
+  ValueNotifier<bool> get themeSwitch =>
+      changeThemeViewModel.config.themeSwitch;
+
+  final ILocalStorage storage = SharedLocalStorageService();
+}
